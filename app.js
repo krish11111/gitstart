524340ce792d18c3b466983ff8748e1a82b65d19
krish11111 <krishnachaubey2799@gmail.com>
2023-04-07 13:53:04 +0530

    some more feature

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
- var items = document.getElementsByClassName('list-group-item');
- console.log(items);
- items[2].style.backgroundColor='green';
+var div=document.createElement('div');
+div.id='krishna';
+div.setAttribute('class','hello');
+var text=document.createTextNode('hello world');
+div.appendChild(text);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+container.insertBefore(div,h1);
 
