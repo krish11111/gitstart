f6065132273c4a26e2d1c60bf718d8eb368953c4
krish11111 <krishnachaubey2799@gmail.com>
2023-04-08 10:44:30 +0530

    some messages

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
-var div=document.createElement('div');
-div.id='krishna';
-div.setAttribute('class','hello');
-var text=document.createTextNode('hello world');
-div.appendChild(text);
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
-container.insertBefore(div,h1);
+
+var form=document.getElementById('addForm');
+var itemlist=document.getElementById('items');
+
+
+
+
+
+
+document.addEventListener('submit',add);
+document.addEventListener('click',remove)
+
+function add(e){
+    e.preventDefault();
+    var text=document.getElementById('item').value;
+    var list=document.createElement('li');
+
+    var btn=document.createElement('button');
+    btn.className='btn btn-danger btn-sm float-right delete';
+    list.className='list-group-item';
+    list.appendChild(document.createTextNode(text));
+    itemlist.appendChild(list);
+    btn.appendChild(document.createTextNode("X"));
+    list.appendChild(btn);
+   }
+function remove(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('are you sure?')){
+            var k=e.target.parentElement;
+           itemlist.removeChild(k);
+        }
+    }
+   
+}
+var delbtn=document.createElement('button');
+delbtn.className="btn btn-danger btn-sm float-right";
+for(var i=0;i<itemlist.length;i++){
+    document.children[i].appendChild(delbtn.appendChild(document.createTextNode('Ed')));
+console.log("krishna");
+}
+
+
+
+
+ 
+
+
 
